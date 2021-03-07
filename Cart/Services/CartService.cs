1f261e9849f48c85ea002d628ce39a24a4e586f2
ahmad <ahmad2smile@protonmail.com>
2021-03-07 17:59:53 +0100

    Add project files.

---
+++ Cart/Services/CartService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Cart.Services
+{
+    public class CartService: CartGrpc.CartGrpcBase
+    {
+        public override Task<TotalReply> GetTotal(TotalRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new TotalReply
+            {
+                Total = 99
+            });
+        }
+    }
+}
---
+++ GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Cart;
+using Grpc.Net.Client;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new CartGrpc.CartGrpcClient(channel);
+
+            var reply = client.GetTotal(new TotalRequest
+            {
+                Id = "1"
+            });
+
+            Console.WriteLine($"Total: {reply.Total}");
+            Console.ReadKey();
+        }
+    }
+}
